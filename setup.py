794890996acf7dda5fcbea22faeae6c2a151282c
Untara Eka Saputra <untara337@gmail.com>
2024-01-19 09:30:49 +0700

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rumuslingkaran33423324',
+    name='rumus33423324',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
